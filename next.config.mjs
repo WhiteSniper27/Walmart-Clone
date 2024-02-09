d4eeff028862ae4e4432df78116494f0553a704b
vaibhav1281 <vaibhav1281saroj@gmail.com>
2024-02-10 02:52:16 +0530

    Completed the landing page and search page. Successfully implemented E-Commerce Scraper API (Oxylabs).

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images:{
+        remotePatterns:[
+            {
+                protocol:"https"  , 
+                hostname: "i5.walmartimages.com",
+            }
+        ]
+    }
+};
 
 export default nextConfig;
